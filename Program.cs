433601c03799e4063b7c7b22cbb2489c3cbabc87
paetalung <paetalung@gmail.com>
2019-06-07 14:00:59 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace demo01.variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 0;
+            int score = 92;
+
+            Console.WriteLine("There is " + money + " Bahts in your wallet.");
+            if (score > 90)
+            {
+                money += 100;
+                Console.WriteLine("Your score is " + score + ".");
+                Console.WriteLine("You got the prize for 100 Baht.");
+            }
+            else if (score >= 80)
+            {
+                money += 50;
+                Console.WriteLine("Your score is " + score + ".");
+                Console.WriteLine("You got the prize for 50 Baht.");
+            }
+            else if (score >= 70)
+            {
+                money += 30;
+                Console.WriteLine("Your score is " + score + ".");
+                Console.WriteLine("You got the prize for 30 Baht.");
+            }
+            else if (score >= 50)
+            {
+                money += 10;
+                Console.WriteLine("Your score is " + score + ".");
+                Console.WriteLine("You got the prize for 10 Baht.");
+            }
+            else
+            {
+                money += 5;
+                Console.WriteLine("Your score is " + score + ".");
+                Console.WriteLine("You got the prize for 5 Baht.");
+            }
+
+            Console.WriteLine("Now,You have " + money + " Baht in your wallet.");
+        }
+    }
+}
+
